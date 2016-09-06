30c08bf598d20c5b25b491947c1518fb13979c0c
Venryx <venryx@gmail.com>
2016-09-06 02:31:44 -0700

    * Tag stripping works again.

--- RestreamChat_Web/js/chat.js
+++ RestreamChat_Web/js/chat.js
@@ -251,7 +251,7 @@ try {
 								
 								// textify any tags that aren't emoji-img's
 								//text = text.replace(/<(?!img src="(http:\/\/static-cdn.jtvnw.net\/|https:\/\/www.livecoding.tv\/|\/chatemoticon\/cache\?))/g, "&lt;");
-								//text = text.replace(/<(?!img src="(http:\/\/static-cdn.jtvnw.net\/|https:\/\/www.livecoding.tv\/|https:\/\/beam.pro\/_latest\/emoticons\/))/g, "&lt;");
+								text = text.replace(/<(?!img src="(http:\/\/static-cdn.jtvnw.net\/|https:\/\/www.livecoding.tv\/|https:\/\/beam.pro\/_latest\/emoticons\/))/g, "&lt;");
 								text = text.replace(/onload/g, "on load").replace(/onerror/g, "on error");
 							}
 							json[i].Text = text;
